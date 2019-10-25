d417f5724c5e052d7b0bd5e0d3f625fae95ecae0
meeronb <47029105+meeronb@users.noreply.github.com>
2019-10-25 07:03:11 +0530

    Update httprobe.sh

--- httprobe.sh
+++ httprobe.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+sudo mysql -sN -e 'select distinct domainname from recondb.domains limit 20' | httprobe -c 20 | sed 's/https\?:\/\///' | xargs -n1 -I{} mysql -e 'update recondb.domains set 'alive' =1 where 'domainname'="{}"'
 
